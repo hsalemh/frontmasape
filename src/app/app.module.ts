7862ff43d2059aac089e32566cb7f648aa872eda
Ismaila SACKO <ismalsacko@yahoo.fr>
2022-05-25 15:13:18 +0200

    Mise en place du code du companent espace Client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QsnComponent } from './qsn/qsn.component';
 import { ConnexionComponent } from './connexion/connexion.component';
-<<<<<<< HEAD
+
 import { SpaceclientComponent } from './spaceclient/spaceclient.component';
-=======
+
 import { HttpClientModule } from '@angular/common/http'
->>>>>>> 7c9a1b3f8cfcd06c8d974c78df8cd72d9e20ac1d
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
+
     ConnexionComponent,
-    SpaceclientComponent
-=======
+    SpaceclientComponent,
+
     QsnComponent,
     ConnexionComponent
->>>>>>> 7c9a1b3f8cfcd06c8d974c78df8cd72d9e20ac1d
+// 7c9a1b3f8cfcd06c8d974c78df8cd72d9e20ac1d
   ],
   imports: [
     BrowserModule,
