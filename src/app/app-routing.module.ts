aaf6ed3c5387e934878078b81457ffd98701d0c3
Ismaila SACKO <ismalsacko@yahoo.fr>
2022-05-26 12:58:17 +0200

    f

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { NosvetementComponent } from './nosvetement/nosvetement.component';
-
-
 import { QsnComponent } from './qsn/qsn.component';
-import {SpaceclientComponent} from "./spaceclient/spaceclient.component";
+import {SpaceclientComponent} from './spaceclient/spaceclient.component';
 
 const routes: Routes = [
 
   {path: 'qsn' , component : QsnComponent},
   {path: 'connexion', component : ConnexionComponent},
-
   {path: 'nosvetement', component : NosvetementComponent},
-
-
   {path:'spaceclient', component:SpaceclientComponent}
 
 ];
