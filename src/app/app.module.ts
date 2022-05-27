4d6c4f65c901f9c131460e8923f15f7dc48581cd
shomakher19 <fremmqnqs@gmail.com>
2022-05-27 12:53:29 +0200

    mise a jour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConnexionComponent
+    ConnexionComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
