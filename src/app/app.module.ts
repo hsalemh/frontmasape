4b5ba75f1392c5cb59f9ceac62096551156ac50a
chatdowcode <ultradiego@msn.com>
2022-05-27 14:50:25 +0200

    misea jour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {QsnComponent} from './qsn/qsn.component';
 import {ConnexionComponent} from './connexion/connexion.component';
 import {HttpClientModule} from '@angular/common/http';
 import {NosvetementComponent} from './nosvetement/nosvetement.component'
-
-
 import {SpaceclientComponent} from './spaceclient/spaceclient.component';
-
-
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
@@ -20,26 +15,14 @@ import { AideComponent } from './aide/aide.component';
 import { PanierComponent } from './panier/panier.component';
 import { ProduitsComponent } from './produits/produits.component';
 
-<<<<<<< HEAD
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ConnexionComponent } from './connexion/connexion.component';
-import { HomeComponent } from './home/home.component';
-=======
->>>>>>> cde0d89ccbb5d95395386dbbfa267576c3a0e834
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    ConnexionComponent,
-    HomeComponent
-=======
-
     ConnexionComponent,
     SpaceclientComponent,
     QsnComponent,
-    ConnexionComponent,
     NosvetementComponent,
     HeaderComponent,
     FooterComponent,
@@ -49,7 +32,6 @@ import { HomeComponent } from './home/home.component';
     ProduitsComponent,
 // 7c9a1b3f8cfcd06c8d974c78df8cd72d9e20ac1d
 
->>>>>>> cde0d89ccbb5d95395386dbbfa267576c3a0e834
   ],
   imports: [
     BrowserModule,
